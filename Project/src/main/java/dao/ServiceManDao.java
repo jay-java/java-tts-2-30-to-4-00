8693635c476164e283d2982658f841de37274306
jay-java <jayprajapati.tops@gmail.com>
2023-07-29 10:45:09 +0530

    latest commit

--- Project/src/main/java/dao/ServiceManDao.java
+++ Project/src/main/java/dao/ServiceManDao.java
@@ -138,4 +138,26 @@ public class ServiceManDao {
 		}
 		return list;
 	}
+	public static ServiceMan getServicemanById(int id) {
+		ServiceMan s1 = null;
+		try {
+			Connection con = DBConnecrtion.createConnection();
+			String sql = "select * from serviceman where id=?";
+			PreparedStatement pst = con.prepareStatement(sql);
+			pst.setInt(1,id);
+			ResultSet rs = pst.executeQuery();
+			if(rs.next()) {
+				s1 = new ServiceMan();
+				s1.setId(rs.getInt("id"));
+				s1.setName(rs.getString("name"));
+				s1.setContact(rs.getLong("contact"));
+				s1.setAddress(rs.getString("address"));
+				s1.setEmail(rs.getString("email"));
+				s1.setPassword(rs.getString("password"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return s1;
+	}
 }
